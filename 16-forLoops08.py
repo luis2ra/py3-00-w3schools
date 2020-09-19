facad8cad5434e63a4a802bb441428006bdae7fb
Luis Altuve <luis2ra@gmail.com>
2020-09-19 17:17:32 -0400

    feat: use of range for looping through a decremental list

--- 16-forLoops08.py
+++ 16-forLoops08.py
@@ -9,3 +9,7 @@ The range() function defaults to increment the sequence by 1, however it is poss
 # Increment the sequence with 3 (default is 1):
 for x in range(2, 30, 3):
     print(x)
+    
+# Decrement the sequence with 2 (default is 1):
+for x in range(30, 0, -2):
+    print(x)
