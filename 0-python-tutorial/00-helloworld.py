d9546243cc0c8924c74fa5294a9484001f3be7c6
Luis Altuve <luis2ra@gmail.com>
2022-12-13 09:48:41 -0500

    updating tutorial

--- 0-python-tutorial/00-helloworld.py
+++ 0-python-tutorial/00-helloworld.py
@@ -33,15 +33,18 @@ Why Python?
 
 Good to know
 
-* The most recent major version of Python is Python 3, which we shall be using in this tutorial. However, Python 2, although not being updated with anything other than security updates, is still quite popular.
-* In this tutorial Python will be written in a text editor. It is possible to write Python in an Integrated Development Environment, such as Thonny, Pycharm, Netbeans or Eclipse which are particularly useful when managing larger collections of Python files.
+* The most recent major version of Python is Python 3, which we shall be using in this tutorial. However, Python 2, although not being updated with 
+anything other than security updates, is still quite popular.
+* In this tutorial Python will be written in a text editor. It is possible to write Python in an Integrated Development Environment, such as Thonny, 
+Pycharm, Netbeans or Eclipse which are particularly useful when managing larger collections of Python files.
 
 
 Python Syntax compared to other programming languages
 
 * Python was designed for readability, and has some similarities to the English language with influence from mathematics.
 * Python uses new lines to complete a command, as opposed to other programming languages which often use semicolons or parentheses.
-* Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes. Other programming languages often use curly-brackets for this purpose.
+* Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes. Other programming languages 
+often use curly-brackets for this purpose.
 
 '''
 
