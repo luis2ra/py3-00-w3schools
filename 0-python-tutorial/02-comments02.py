72b26f926b9ce7f7b848facf3c91ae908af9b68d
Luis Altuve <luis2ra@gmail.com>
2022-12-13 10:53:59 -0500

    updated python comments topic

--- 0-python-tutorial/02-comments02.py
+++ 0-python-tutorial/02-comments02.py
@@ -1,3 +1,5 @@
+# @author: https://github.com/luis2ra from https://www.w3schools.com/python/python_comments.asp
+
 # Demo Python Comments
 
 # Comments can be placed at the end of a line, and Python will ignore the rest of the line:
--- 0-python-tutorial/02-comments03.py
+++ 0-python-tutorial/02-comments03.py
@@ -1,3 +1,5 @@
+# @author: https://github.com/luis2ra from https://www.w3schools.com/python/python_comments.asp
+
 # Demo Python Comments
 
 # Comments does not have to be text to explain the code, it can also be used to prevent Python from executing code:
