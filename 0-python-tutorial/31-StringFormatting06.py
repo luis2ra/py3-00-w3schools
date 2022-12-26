2fe4e7a4b96b9f496ae709b43fa95ac85a1ac801
Luis Altuve <luis2ra@gmail.com>
2022-12-26 10:36:41 -0500

    files deleted

--- 0-python-tutorial/31-StringFormatting06.py
+++
@@ -1,6 +0,0 @@
-# Demo Python String Formatting - Named Indexes
-
-# You can also use named indexes by entering a name inside the curly brackets {carname}, 
-# but then you must use names when you pass the parameter values txt.format(carname = "Ford"):
-myorder = "I have a {carname}, it is a {model}."
-print(myorder.format(carname = "Ford", model = "Mustang"))
