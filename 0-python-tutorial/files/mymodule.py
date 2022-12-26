7f4e40f364c58f6ea44660831f998093b4a90841
Luis Altuve <luis2ra@gmail.com>
2022-12-26 10:20:22 -0500

    updated "Python Modules" topic (minor change)

--- 0-python-tutorial/files/mymodule.py
+++ 0-python-tutorial/files/mymodule.py
@@ -1,2 +1,6 @@
+# @author: https://github.com/luis2ra from https://www.w3schools.com/python/python_modules.asp
+
+# Demo of file for Python Modules
+
 def greeting(name):
-  print("Hello, " + name)
+	print("Hello, " + name)
