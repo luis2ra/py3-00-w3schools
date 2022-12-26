1d2a63004c28481f8119846cabe790c6c09f92ac
Luis Altuve <luis2ra@gmail.com>
2022-12-26 15:36:26 -0500

    updated "Python User Input" topic

---
+++ 0-python-tutorial/31-UserInput01.py
@@ -0,0 +1,26 @@
+# @author: https://github.com/luis2ra from https://www.w3schools.com/python/python_user_input.asp
+
+# Demo Python User Input
+'''
+Python User Input - User Input
+
+
+Python allows for user input.
+
+That means we are able to ask the user for input.
+
+The method is a bit different in Python 3.6 than Python 2.7.
+
+Python 3.6 uses the input() method.
+
+Python 2.7 uses the raw_input() method.
+
+The following example asks for the username, and when you entered the username, it gets printed on the screen.
+
+Note: Python stops executing when it comes to the input() function, and continues when the user has given some input.
+
+'''
+
+# Python 3.9
+username = input("Enter username: ")
+print("Username is: " + username)
