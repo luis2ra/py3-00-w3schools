e303d7a1f3054285a9ae72702ca684e19d0ed5c9
Luis Altuve <luis2ra@gmail.com>
2023-01-26 16:27:15 -0500

    solving notification of GitGuardian

--- 4-python-django/my_tennis_club/my_tennis_club/settings.py
+++ 4-python-django/my_tennis_club/my_tennis_club/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u_6k8-qxtgj9(ku*1d0ih&s^&nur@4g^98l907qih-e)d3hd-h'
+SECRET_KEY = environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
