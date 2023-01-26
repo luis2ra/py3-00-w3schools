fbb8fe576b89d2b2426ca6d5e258d4cebd15fdf5
Luis Altuve <luis2ra@gmail.com>
2023-01-26 17:41:35 -0500

    "Django Templates" topic

---
+++ 4-python-django/1-Django/09-django-templates.py
@@ -0,0 +1,15 @@
+# @author: https://github.com/luis2ra from https://www.w3schools.com/django/django_templates.php
+
+'''
+Django Templates
+
+
+Templates
+
+In the Django Intro page, we learned that the result should be in HTML, and it should be created in a template, so let's do that.
+
+Create a templates folder inside the members folder, and create a HTML file named myfirst.html.
+
+
+'''
+print("Django Templates")
--- 4-python-django/my_tennis_club/members/views.py
+++ 4-python-django/my_tennis_club/members/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 
 # Create your views here.
 def members(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
