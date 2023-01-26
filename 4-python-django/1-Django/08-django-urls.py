d9695bbd2533a72fc468134635df738c8e8a38c1
Luis Altuve <luis2ra@gmail.com>
2023-01-26 17:29:40 -0500

    "Django URLs" topic

---
+++ 4-python-django/1-Django/08-django-urls.py
@@ -0,0 +1,16 @@
+# @author: https://github.com/luis2ra from https://www.w3schools.com/django/django_urls.php
+
+'''
+Django URLs
+
+
+Create a file named my_tennis_club/members/urls.py in the same folder as the views.py file.
+
+The urls.py file you just created is specific for the members application. We have to do some routing 
+in the root directory my_tennis_club as well. This may seem complicated, but for now, just follow the instructions below.
+
+There is a file called urls.py on the my_tennis_club folder, open that file and add the include module in the import statement, 
+and also add a path() function in the urlpatterns[] list, with arguments that will route users that comes in via 127.0.0.1:8000/.
+
+'''
+print("Django URLs")
--- 4-python-django/my_tennis_club/my_tennis_club/settings.py
+++ 4-python-django/my_tennis_club/my_tennis_club/settings.py
@@ -21,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get("SECRET_KEY")
-print("environ ", dict(environ))
+SECRET_KEY = environ.get("SECRET_KEY", "django-insecure-u_6k8-qxtgj9(ku*1d0ih&s^&nur@4g^98l907qih-e)d3hd-h")
+print("environ ", SECRET_KEY)
+#print(dict(environ))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
