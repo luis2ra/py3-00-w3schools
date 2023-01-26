08e134ede7cc474bc11a9c1ce519417b49193c24
Luis Altuve <luis2ra@gmail.com>
2023-01-26 17:18:29 -0500

    settings modification

--- 4-python-django/my_tennis_club/my_tennis_club/settings.py
+++ 4-python-django/my_tennis_club/my_tennis_club/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+# Standard Library
 from pathlib import Path
 from os import environ
 
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get('SECRET_KEY', None)
+SECRET_KEY = environ.get("SECRET_KEY")
+print("environ ", dict(environ))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
