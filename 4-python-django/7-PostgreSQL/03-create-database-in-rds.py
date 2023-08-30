739c5eb8d68caf8c22de49fd82f1b09822dca2f8
Luis Altuve <luis2ra@gmail.com>
2023-08-29 23:57:51 -0500

    updated PostgreSQL topic from Django Tutorial

---
+++ 4-python-django/7-PostgreSQL/03-create-database-in-rds.py
@@ -0,0 +1,4 @@
+# @author: https://github.com/luis2ra from https://www.w3schools.com/django/django_db_create_rds.php
+
+
+print("Create PostgreSQL Database")
---
+++ 4-python-django/7-PostgreSQL/05-add-members.py
@@ -0,0 +1,4 @@
+# @author: https://github.com/luis2ra from https://www.w3schools.com/django/django_db_add_members.php
+
+
+print("PostgreSQL - Add Members")
